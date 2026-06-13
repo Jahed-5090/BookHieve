27a72ef2fb5daad917636309dde6d31c010f700b
Intejar <intejaralif@gmail.com>
2026-06-13 23:29:50 +0600

    nothing

--- main.cpp
+++ main.cpp
@@ -1,19 +1,3 @@
-// +--------------------------------------------------------------------------╗
-// |       BookHieve - Digital Library Management System                     |
-// |       DSA Project in C++                                                |
-// |                                                                          |
-// |  DSA Structures Used:                                                    |
-// |   • Array Operations      - BorrowHistory (vector + merge sort)          |
-// |   • Stack                 - BorrowStack (undo last borrow)               |
-// |   • Queue                 - BorrowQueue (waitlist FIFO)                  |
-// |   • Infix→Postfix/Prefix  - Expression evaluator (fine formula)          |
-// |   • Singly Linked List    - UserList membership                          |
-// |   • BST                   - BookBST catalogue search/insert/delete       |
-// |   • Sorting (5 kinds)     - Bubble/Insertion/Selection/Quick/Merge       |
-// |   • Heap sort/min/max     - FineMaxHeap, DueMinHeap                      |
-// |   • BFS / DFS / Topo Sort - GenreGraph recommendations                   |
-// +--------------------------------------------------------------------------╝
-
 #include "include/library.h" 
 
 RecommendationEngine recEngine;
